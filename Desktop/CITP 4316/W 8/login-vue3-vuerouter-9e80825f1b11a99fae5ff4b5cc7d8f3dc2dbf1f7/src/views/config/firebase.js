bc768aaee9bce9ac21b353dc543a7ff85b9a9516
deyaniradle <77951576+deyaniradle@users.noreply.github.com>
2021-03-28 21:05:14 -0500

    GIF FinderApp

---
+++ Desktop/CITP 4316/W 8/login-vue3-vuerouter-9e80825f1b11a99fae5ff4b5cc7d8f3dc2dbf1f7/src/views/config/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBrCp99i98qV-p3MYE48PvRw0PBeZNxc24",
+  authDomain: "loginexamplevite1.firebaseapp.com",
+  projectId: "loginexamplevite1",
+  storageBucket: "loginexamplevite1.appspot.com",
+  messagingSenderId: "306687295174",
+  appId: "1:306687295174:web:eb3b8d9f46be9eca66a1c7"
+};
